b4dca56352b5f4afdd51458368135f15cc28cd0c
SophWennersten <sophwennersten@hotmail.com>
2025-09-18 18:22:17 +0200

    Adding LineardGradient inside Pressable

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,35 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { router } from "expo-router";
 import { Pressable, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import RandomPhrases from "../components/random-phrases-homescreen";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Pressable
-        style={({ pressed }) => [
-          styles.button,
-          pressed && {
-            transform: [{ scale: 0.96 }],
-          },
-        ]}
-        onPress={() => router.push("NextScreen")} //Change this!!!
-      >
-        <RandomPhrases />
-      </Pressable>
-    </View>
+    <SafeAreaView style={styles.container} edges={["top"]}>
+      {/* <Header /> */}
+
+      <View style={{ flex: 0.4 }} />
+      <View>
+        <Pressable
+          style={({ pressed }) => [
+            pressed && {
+              transform: [{ scale: 0.96 }],
+            },
+          ]}
+          onPress={() => router.push("NextScreen")} //Change this!!!
+        >
+          <LinearGradient
+            colors={["rgba(184, 134, 11, 0.05)", "rgba(184, 134, 11, 0.2)"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.button}
+          >
+            <RandomPhrases />
+          </LinearGradient>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -24,6 +37,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#000",
+    alignItems: "center",
+  },
+  mainContainer: {
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
   },
