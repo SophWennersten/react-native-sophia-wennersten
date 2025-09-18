40b3479ae32b14632575cddcb386e00d7ed09d69
SophWennersten <sophwennersten@hotmail.com>
2025-09-18 16:52:43 +0200

    Added pressable  & routing.push + style

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, View } from "react-native";
+import { router } from "expo-router";
+import { Pressable, StyleSheet, View } from "react-native";
 import RandomPhrases from "../components/random-phrases-homescreen";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <RandomPhrases />
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed && {
+            transform: [{ scale: 0.96 }],
+          },
+        ]}
+        onPress={() => router.push("NextScreen")} //Change this!!!
+      >
+        <RandomPhrases />
+      </Pressable>
     </View>
   );
 }
@@ -15,5 +26,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#000",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: "#B8860B",
+    backgroundColor: "transparent",
+    margin: 30,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderRadius: 8,
   },
 });
