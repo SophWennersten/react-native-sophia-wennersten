ac73a47411a6a00e7a43d0edea2e4dae0498aca9
SophWennersten <sophwennersten@hotmail.com>
2025-09-18 18:45:00 +0200

    Added ImageBackground with some LinearGradient to _layout.tsx for global background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
 import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { View } from "react-native";
+import { ImageBackground, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Header from "../components/header";
 
@@ -23,15 +24,30 @@ export default function RootLayout() {
   }
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-        <View style={{ alignItems: "center" }}>
-          <Header />
-        </View>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="NextScreen" />
-        </Stack>
-      </SafeAreaView>
+      <ImageBackground
+        source={require("../assets/images/img-stars.jpg")}
+        style={{ flex: 1 }}
+      >
+        <LinearGradient
+          colors={["rgba(0,0,0,0.8)", "rgba(0,0,0,0.6)", "rgba(0,0,0,0.2)"]}
+          style={{ flex: 1 }}
+        >
+          <SafeAreaView style={{ flex: 1 }}>
+            <View style={{ alignItems: "center" }}>
+              <Header />
+            </View>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: { backgroundColor: "transparent" },
+              }}
+            >
+              <Stack.Screen name="index" />
+              <Stack.Screen name="NextScreen" />
+            </Stack>
+          </SafeAreaView>
+        </LinearGradient>
+      </ImageBackground>
     </SafeAreaProvider>
   );
 }
