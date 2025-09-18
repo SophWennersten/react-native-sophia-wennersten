ad8a64eaab18b726e7cc839b7d5b1b766499e392
SophWennersten <sophwennersten@hotmail.com>
2025-09-18 15:46:08 +0200

    Initial set up: removed files + adding initial structure + add expo SDK: font, splashscreen and navigation

---
+++ app/_layout.tsx
@@ -0,0 +1,26 @@
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    GermaniaOne: require("../assets/fonts/GermaniaOne-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        Open up App.tsx to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    color: "#fff",
+    fontFamily: "GermaniaOne",
+  },
+});
